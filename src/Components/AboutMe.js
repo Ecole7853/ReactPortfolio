e657725585c1ee64436a04bb04286506fa727af5
Eric <ecole052785@gmail.com>
2022-02-20 17:01:32 -0500

    css adjustments

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,24 +5,25 @@ function AboutMe() {
     console.log(window.location.href)
     return (
         <>
-             <Header />
+            <Header />
             <div className="top">
                 <div id="aboutMe">
-                <h2>About Me</h2>
-                <div id="imgContainer">
-                    <img className="pic" src= "./images/Face.jpg"/>
-                    </div>  
+                    <div id="h2Box">
+                        <h2>About Me</h2>
+                        <div id="imgContainer">
+                            <img className="pic" src="./images/Face.jpg" />
+                        </div>
                         <div id="about">
-                        <p id="aboutText">
-                                Hello world! 
-                                    My name is Eric Cole, thanks for viewing my portfolio! While here feel free to check out some of my other projects which can be seen below!
+                            <p id="aboutText">
+                                Hello world!
+                                My name is Eric Cole, thanks for viewing my portfolio! While here feel free to check out some of my other projects which can be seen below!
                             </p>
                         </div>
-                        
+                    </div>
                 </div>
             </div>
         </>
-                )
+    )
 }
 
 export default AboutMe;
