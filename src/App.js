416b8ddd487603d3259fcbf0cda1f38eb07a06d6
Eric <ecole052785@gmail.com>
2022-02-20 16:01:26 -0500

    removed redundent pages, consolidating

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React from "react";
-import Home from "./Components/Home";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import ProfileContainer from "./Components/ProfileContainer";
 import ContactMe from "./Components/ContactMe";
 import Portfolio from "./Components/Portfolio";
 import AboutMe from "./Components/AboutMe";
@@ -15,7 +13,7 @@ function App() {
   <Router>
     <div>
       <Route exact path="/"> 
-      <Home />
+      <AboutMe />
       </Route>
       <Route exact path="/AboutMe"> 
       <AboutMe />
@@ -28,10 +26,7 @@ function App() {
       </Route>
       <Route exact path="/Portfolio"> 
       <Portfolio />
-      </Route>
-      <Route exact path="/ProfileContainer"> 
-      <ProfileContainer />
-      </Route>   
+      </Route> 
       <Footer />
     </div>
     </Router> 
