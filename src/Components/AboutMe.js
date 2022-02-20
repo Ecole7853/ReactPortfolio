767a4c25040cd88f3bb08fc61a190604d905576e
Eric <ecole052785@gmail.com>
2022-02-20 16:18:38 -0500

    removed poor syntax issue, changed about me statement, continuing to prep to consolidate all content on one singular page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -14,10 +14,8 @@ function AboutMe() {
                     </div>  
                         <div id="about">
                         <p id="aboutText">
-                                My name is Eric Cole, I'm currently a foreman with an electrical company and work out of DC, I'm
-                                also in a bootcamp through Trilogy learning Full stack development. My goals over the next few
-                                years are to become a fullstack developer! Below, is some of the work I've accomplished within
-                                the course.
+                                Hello world! 
+                                    My name is Eric Cole, thanks for viewing my portfolio! While here feel free to check out some of my other projects which can be seen below!
                             </p>
                         </div>
                         
