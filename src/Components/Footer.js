8aa73007401ea02bd97068271121467d71894f76
Eric <ecole052785@gmail.com>
2022-02-20 12:36:52 -0500

    fixed linkedin link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <>
         <footer>
             <a href="https://github.com/Ecole7853" id="gitHub">My GitHub</a> 
-            <a href="https://linkedin.com/ecole7853/" id="linkedIn">My LinkedIn</a> 
+            <a href="https://linkedin.com/in/ecole7853/" id="linkedIn">My LinkedIn</a> 
             <a href="https://www.facebook.com/eric.cole.980" id="facebook">My facebook</a>
         </footer>
         </> 
