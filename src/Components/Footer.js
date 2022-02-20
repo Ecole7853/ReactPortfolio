daa4d779c5e24ee446155f6a30553436602672f4
Eric <ecole052785@gmail.com>
2022-02-20 15:39:13 -0500

    adding css to footer buttons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return(
         <>
-        <footer>
+        <footer id="botFooter">
             <a href="https://github.com/Ecole7853" id="gitHub">My GitHub</a> 
             <a href="https://linkedin.com/in/ecole7853/" id="linkedIn">My LinkedIn</a> 
             <a href="https://www.facebook.com/eric.cole.980" id="facebook">My facebook</a>
