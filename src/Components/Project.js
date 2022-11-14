7a594b5a42891dca34af583fcc565a0c2d7a3099
eric cole <ecole052785@gmail.com>
2022-11-13 21:25:05 -0500

    commenting pageflip out, heroku was crashing

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import HTMLFlipBook from "react-pageflip";
+// import HTMLFlipBook from "react-pageflip";
 
 function Project() {
   console.log();
@@ -12,12 +12,12 @@ function Project() {
         exit={{ opacity: 0 }}
         transition={{ duration: 1.5 }}
       >
-        <HTMLFlipBook width={600} height={1200} className="workBook">
+        {/* <HTMLFlipBook width={600} height={1200} className="workBook">
           <div className="workBookPage1">Page 1</div>
           <div className="workBookPage2">Page 2</div>
           <div className="workBookPage3">Page 3</div>
           <div className="workBookPage4">Page 4</div>
-        </HTMLFlipBook>
+        </HTMLFlipBook> */}
       </motion.div>
     </>
   );
