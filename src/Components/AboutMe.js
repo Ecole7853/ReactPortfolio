83018e3ec5296af0bead0c0f797c0165e57f23f5
eric cole <ecole052785@gmail.com>
2022-11-28 11:49:29 -0500

    Fixed up about me, continued on css for menu button media query

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import me from "../images/mahFacePartDuex.jpg"
-import family from "../images/family.jpg"
-import family2 from "../images/family4.jpg"
-import cityridge from "../images/cityridge.jpg"
-import car from "../images/car.jpeg"
-import gym from "../images/gym.jpg"
-
 
 function AboutMe() {
   console.log(window.location.href);
@@ -18,44 +12,15 @@ function AboutMe() {
         exit={{ opacity: 0 }}
         transition={{ duration: 1.5 }}
       >
-        <div id="aboutCont" className="flex flex-row">
-          <div id="fullCont1" className="flex-1 flex-col mt-5 ml-5 mx-auto">
-            <div id="contOne" className="flex flex-row space-x-10 mb-5">
-              <div id="imageOne" className="max-w-xs opacity-50 ml-10"><img src={me}>
-              </img></div>
-              <p id="textOne" className="text-white text-2xl max-w-lg text-blue-300 text-center space-x-10 mt-10">Hello, my name is Eric, thanks for visiting my portfolio, this is the about me page where we pretend I&apos;m super interesting and you&apos;re absolutely captivated. I&apos;m a software engineer with Trilogy working at the University of Conneticut.</p>
-            </div>
-            <div id="contTwo" className="flex flex-row my-5">
-              <div id="imageTwo" className="max-w-xs ml-10 mr-20 opacity-50">
-                <img src={family}>
-                </img>
+        <div id="aboutCont" className="">
+          <div id="fullCont1" className="flex justify-around">
+            <div id="contOne" className="flex">
+              <div id="imageOne" className="max-w-md opacity-50 mt-12 mr-12">
+                <img className=" rounded-lg"src={me}>
+              </img>
               </div>
-              <div id="imageTwo2" className="max-w-sm ml-5 opacity-50">
-                <img src={family2}>
-                </img>
-              </div>
-            </div>
-          </div>
-          <div id="fullCont2" className="flex-1 flex-col mx-auto">
-            <div id="contThree" className="flex flex-row my-5">
-              <div id="imageThree" className="max-w-xs opacity-50 mt-5"> <img src={cityridge}>
-              </img></div>
-              <p id="textThree" className="text-white text-2xl text-blue-300 max-w-lg text-center space-x-10 mb-10 ml-5">This is City Ridge, it&apos;s a series of multi-family dwelling units in Washington, DC that I worked on as my last project at my previous career, the building in the bottom right of the picture was mine! You can see a virtual tour, showing the size and scope of this project by googling &quot;City Ridge&quot;</p>
-            </div>
-            <div id="fullCont3" className="flex flex-row">
-            <div id="contFour" className="flex flex-col my-5">
-              <p id="textFour" className="text-white text-2xl max-w-md text-blue-300 text-center mr-5 mt-5 mb-10">This is one of my hobbies, I also train Jiu Jitsu and have trained for Marylands strongest man competition at Colosseum Gym in Columbia.</p> 
-              <div id="imageFour" className="max-w-xs ml-10 opacity-50 mb-20">
-                <img src={car}>
-                </img>  
-                </div>
-            </div>
-            <div id="contFive" className="flex flex-col my-5">
-              <div id="imageFive" className="max-w-xs mt-5 ml-5 opacity-50">
-                <img src={gym}>
-                </img>
-              </div>
-            </div>
+              <p id="textOne" className="ml-10 text-xl max-w-xl text-blue-300 text-center space-x-10 mt-10">Hello, my name is Eric, and I live in Maryland. I&apos;ve been coding for about 2 years mostly in JavaScript. I&apos;ve come to enjoy MERN stack the most. Which is why I remade my portfolio in react. I picked up coding because it seemed like a fun hobby, so I took a bootcamp and have been coding since. I work with University of Connecticut now, as a fullstack educator. I quit my job as an electrical superintendant to further persue this career. That job had a lot of project management, I was in charge of up to 50 people depending on project phase, and was responsible for communicating with the project leads. My last project was the E2 building of City Ridge, you can actually see a virtual 3d walkthrough of the project on youtube. I&apos;m currently working on a forum page that can be easily changed based on the customers needs. Utilizing MERN, this page will feature react components that can easily be renamed and purposed for sub forums, making the project easily reused for each individual customer. Other, already completed projects, can be seen in the projects tab of this portfolio.
+              </p>
             </div>
           </div>
         </div>
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 import pdf from "../assets/resumewcover.pdf";
 import home from "../images/home.png";
 
-function Navigation() {
+
+
+//function Navigation() {
+class Navigation extends React.Component{
+  handleClick(){
+    document.getElementById("menu").style.display = "block";
+  }
+
+render(){
+
   return (
     <>
-      <nav className="bg-gradient-to-r from-gray-600 to-neutral-900 flex-1">
+      <nav className=" bg-gradient-to-r from-gray-600 to-neutral-900 flex-1">
         <div id="buttonSeperation" className="flex flex-column justify-between">
-           <ul id="homeButton" className="h-20 mb-3 mt-7 my-5 pt-3 pb-4 pl-3 pr-4 rounded-md transition-all
+          <ul id="homeButton" className="hidden h-20 mb-3 mt-7 my-5 pt-3 pb-4 pl-3 pr-4 rounded-md transition-all
    duration-1000 
-   ease-in-out hover:bg-zinc-400"> 
+   ease-in-out hover:bg-zinc-400 md:block">
             <li>
               <Link to="/">
                 <img src={home}>
@@ -20,7 +29,7 @@ function Navigation() {
           </ul>
           <ul
             id="navItems"
-            className="flex flex-row justify-between align-between p-4 m-4"
+            className="md:inline-flex flex flex-row justify-between align-between p-4 m-4 hidden"
           >
             <li id="buttons" className="text-2xl font-medium text-gray-500 pt-4 px-2 mx-1 pb-4 rounded-md transition-all
    duration-1000 
@@ -35,7 +44,7 @@ function Navigation() {
             <li id="buttons" className="text-2xl font-medium text-gray-500 pt-4 px-2 mx-1 pb-4 rounded-md transition-all
    duration-1000 
    ease-in-out hover:bg-zinc-600 hover:text-black">
-            <Link to="/Contact">Contact Me</Link>
+              <Link to="/Contact">Contact Me</Link>
             </li>
             <li id="buttons" className="text-2xl font-medium text-gray-500 pt-4 px-2 mx-1 pb-4 rounded-md transition-all
    duration-1000 
@@ -45,10 +54,28 @@ function Navigation() {
               </a>
             </li>
           </ul>
+          <div onClick={this.handleClick} className="md:hidden w-30 z-10">
+            <svg className="w-24 flex align-center justify-center pl-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+            <ul id="menu" className="hidden">
+              <li className="text-2xl text-black list-none flex whitespace-nowrap bg-zinc-600"><Link to="/">Home</Link></li>
+              <li className="text-2xl text-black list-none flex whitespace-nowrap bg-zinc-600"><Link to="/AboutMe">About</Link></li>
+              <li className="text-2xl text-black list-none flex whitespace-nowrap bg-zinc-600"><Link to="/Project">Projects</Link></li>
+              <li className="text-2xl text-black list-none flex whitespace-nowrap bg-zinc-600"><Link to="/Contact">Contact Me</Link></li>
+              <li className="text-2xl text-black list-none flex whitespace-nowrap bg-zinc-600">
+                <a href={pdf} target="_blank" rel="noreferrer">
+                Resume
+                </a>
+            </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
   );
 }
+}
+
 
 export default Navigation;
